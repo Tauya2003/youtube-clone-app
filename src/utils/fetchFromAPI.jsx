e54abcbc1960be6e5f9e0c12b8d30bc9f6337289
Tauya <ayuat2003@gmail.com>
2023-07-15 00:17:22 +0200

    Version 1 complete

---
+++ src/utils/fetchFromAPI.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: "100",
+  },
+  headers: {
+    // "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": "8d0c982053msh15b76b3110ae898p1b6c1cjsn08dc667f29bd",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
